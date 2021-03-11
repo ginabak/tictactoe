3ef24b7588dc4cc86ca429a8e65ed647577350b8
ginabak <gina@ginabak.com>
2021-03-11 11:58:31 -0500

    even grids

--- components/Square/Square.tsx
+++ components/Square/Square.tsx
@@ -1,5 +1,5 @@
 import * as styles from "./Square.module.css";
 
 export function Square() {
-	return <div className={styles["Square"]}>hello</div>;
+	return <div className={styles["Square"]}></div>;
 }
